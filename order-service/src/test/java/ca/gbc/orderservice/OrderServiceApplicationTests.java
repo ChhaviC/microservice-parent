4aabcc4293a0d291aa6e8cb1563df0afdb6eaba1
ChhaviC <chhavichhabra5@gmail.com>
2024-11-23 23:35:51 -0500

    Initial commit

---
+++ order-service/src/test/java/ca/gbc/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,32 @@
+package ca.gbc.orderservice;
+
+import ca.gbc.orderservice.client.InventoryClient;
+import ca.gbc.orderservice.stub.InventoryClientStub;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OrderServiceApplicationTests {
+	@Test
+	void contextLoads() {}
+
+}
+
